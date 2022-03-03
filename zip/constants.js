eee3c53fb3ee87461a0a0556d720fd5fcd127864
sinjin25 <raionus2@gmail.com>
2022-03-03 01:42:19 -0800

    Add zip utility

---
+++ zip/constants.js
@@ -0,0 +1,13 @@
+const WARNINGS = {
+    MISSING_INPUT_CATEGORY: 'No category folder selected. Aborting.',
+    MISSING_INPUT_ARCHIVE: 'No archive name presented. Ending.',
+    DUPLICATE_ARCHIVE_FILE: 'Archive already exists. Aborting.',
+    REQUESTED_ABORT: 'User requested an abort.',
+}
+
+module.exports = {
+    ZLIB_BEST_COMPRESSION: 9,
+    NO_CHOICE_SELECTED: -1,
+    BOOK_TYPES: ['Books', 'Anime', 'Drama', 'Games', 'Other'],
+    WARNINGS,
+}
