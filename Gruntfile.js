9aa460ee67499122f53f49fc7294d0a697f96862
sinjin25 <raionus2@gmail.com>
2022-04-25 08:00:20 -0700

    document outward-facing tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,4 @@
-// global config
-/* const globalConfig = require('./grunt-scripts/globalConfig.json')
-
-const amazonDownload = require('./grunt-scripts/amazon-download/index.js') */
+require('colors')
 /**
  * @param {Igrunt} grunt
  */
@@ -22,7 +19,32 @@ module.exports = function(grunt) {
     grunt.loadTasks('./grunt-scripts/tasks')
 
     grunt.registerTask('default', function() {
-        grunt.log.writeln('grunt is working')
+        grunt.log.writeln('Grunt is working. Tasks to run directly include:')
+        const TASKS = {
+            DOWNLOAD_AMAZON: {
+                task: "grunt download-amazon".green,
+                description: `Download a (product) page from amazon. Follow up with the extract-amazon script`.blue
+            },
+            EXTRACT_AMAZON: {
+                task: "grunt extract-amazon".green,
+                description: `Extract the book meta out of an amazon page. Use before 'move-files' etc.`.blue
+            },
+            RENAME_RAW_FILES: {
+                task: "grunt rename-raw-files".green,
+                description: `Rename the output files from mecab analysis (readability report, word freq report) and the book cover (cover.xxx) into a specific format`.blue
+            },
+            MOVE_FILES: {
+                task: "grunt move-files".green,
+                description: `Move the renamed files from 'rename-raw-files' to the correct directories for the application (MYSQL upload folder, cover folder, etc.)`.blue
+            },
+        }
+        const keys = Object.keys(TASKS)
+        keys.forEach((aKey) => {
+            const { task, description } = TASKS[aKey]
+            grunt.log.writeln(`* ${task}:`)
+            grunt.log.writeln(description)
+            grunt.log.writeln(`--------------`)
+        })
     })
 
     grunt.registerTask('test-prompts', function() {
